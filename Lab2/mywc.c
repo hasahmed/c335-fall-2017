b9e79824cd8d7b479d05c88a113015c14d9ddaac
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-08-31 18:46:33 -0400

    lab2

---
+++ Lab2/mywc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int c;
+    int lines = 0;
+    int words = 0;
+    int bytes = 0;
+    int previous_c;
+    while(c != EOF){
+        c = getchar();
+        switch(c){
+            case '\n':
+                if(previous_c != ' '){
+                    words++;
+                }
+                lines++;
+                bytes++;
+                break;
+            case ' ':
+                if(previous_c < 255 && previous_c != ' '
+                        && previous_c != '\t'
+                        && previous_c != '\r'
+                        && previous_c != '\f'
+                        && previous_c != '\v'
+                        && previous_c != '\n'
+                        ){
+                    words++;
+                }
+                bytes++;
+                break;
+            case '\t':
+            case '\r':
+            case '\f':
+            case '\v':
+                bytes++;
+                break;
+            default:
+                bytes++;
+        }
+        previous_c = c;
+    }
+    printf("\t%d\t%d\t%d\n", lines, words, bytes - 1);
+}

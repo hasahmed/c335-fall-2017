904f2656a28b3d381214d425f621ff6dc14fb3a5
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-08-31 19:03:18 -0400

    up to poly

---
+++ Lab2/dump.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//
+// dump.c
+//
+
+void dump_memory(void *p, int len)
+{
+  int i;
+
+  for (i = 0; i < len; i++) {
+    printf("%8p: %02x\n", p + i, *(unsigned char *)(p + i));
+  }
+}

80d2c17a6da81c90bb239e7d7a08b1ab09376a16
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-09-07 09:44:10 -0400

    removed some unnecessary files

--- Lab2/poly/sizes.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("int: %lu\n", sizeof(int));
-    printf("char: %lu\n", sizeof(char));
-    printf("short: %lu\n", sizeof(short));
-    printf("float: %lu\n", sizeof(float));
-    printf("double: %lu\n", sizeof(double));
-}

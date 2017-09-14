28761e3dc390fa2a70701241ad05e057ca9741ae
Hasan Yusuf Ahmed <hasahmed@umail.iu.edu>
2017-09-14 19:34:02 -0400

    worked all the kinks out of 'getchar' 'putchar' 'putstring' for lab4

--- lab4/main.c
+++ lab4/main.c
@@ -7,21 +7,27 @@
 
 // Simple looping delay function
 void delay(void) {
-  int i = 2000000;
-  while (i-- > 0) {
-    asm("nop"); 
-  }
+    int i = 2000000;
+    while (i-- > 0) {
+        asm("nop"); 
+    }
 }
 
 int main(void) {
-void f3d_uart_init();
-  while(1);
+
+    f3d_uart_init();
+
+    while(1){
+        //putchar('a');
+        //putchar(getchar());
+        putstring("hello");
+    }
 }
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
-/* Infinite loop */
-/* Use GDB to find out why we're here */
-  while (1);
+    /* Infinite loop */
+    /* Use GDB to find out why we're here */
+    while (1);
 }
 #endif

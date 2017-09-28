8454667bb2773ccdaa2bed85883a59a121adf52e
Hasan Yusuf Ahmed <hasahmed@umail.iu.edu>
2017-09-28 19:50:15 -0400

    just need to make the application

--- lab6/main.c
+++ lab6/main.c
@@ -1,10 +1,10 @@
 /* main.c ---
-*
-* Filename: main.c
-* Description:
-* Author: Hasan Y Ahmed
-* Maintainer:
-* Created: Thu Jan 10 11:23:43 2013
+ *
+ * Filename: main.c
+ * Description:
+ * Author: Hasan Y Ahmed
+ * Maintainer:
+ * Created: Thu Jan 10 11:23:43 2013
 /* Code: */
 
 #include <stm32f30x.h> // Pull in include files for F30x standard drivers
@@ -17,16 +17,38 @@
 #define TIMER 20000
 
 int main(void) {
-  // If you have your inits set up, this should turn your LCD screen red
-  f3d_lcd_init();
-  f3d_lcd_fillScreen(RED);
+    // If you have your inits set up, this should turn your LCD screen red
+    f3d_lcd_init();
+    while(1){
+
+
+        f3d_lcd_drawChar(10, 10, 'H', RED, BLACK);
+        /*
+        int TIME = 2000;
+        f3d_lcd_fillScreen2(RED);
+        delay(TIME);
+        f3d_lcd_fillScreen2(BLACK);
+        delay(TIME);
+        f3d_lcd_fillScreen2(BLUE);
+        delay(TIME);
+        f3d_lcd_fillScreen2(CYAN);
+        delay(TIME);
+        f3d_lcd_fillScreen2(GREEN);
+        delay(TIME);
+        f3d_lcd_fillScreen2(MAGENTA);
+        delay(TIME);
+        f3d_lcd_fillScreen2(YELLOW);
+        delay(TIME);
+        f3d_lcd_fillScreen2(WHITE);
+        */
+    }
 }
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
-/* Infinite loop */
-/* Use GDB to find out why we're here */
-  while (1);
+    /* Infinite loop */
+    /* Use GDB to find out why we're here */
+    while (1);
 }
 #endif
 

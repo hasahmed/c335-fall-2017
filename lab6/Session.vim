dafea466feed742ffedfe839109c896fec00feca
Hasan Yusuf Ahmed <hasahmed@umail.iu.edu>
2017-10-05 17:26:46 -0400

    made some progress on lab

--- lab6/Session.vim
+++ lab6/Session.vim
@@ -8,16 +8,17 @@ if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
 set shortmess=aoO
-badd +0 main.c
-badd +0 Makefile
+badd +1 main.c
+badd +11 Makefile
 badd +13 ../driver/src/f3d_lcd_sd.c
 badd +14 ../driver/inc/f3d_lcd_sd.h
-badd +0 ../driver/src/f3d_gyro.c
+badd +1 ../driver/src/f3d_gyro.c
 badd +19 ../fall2017-lab/driver/src/f3d_delay.c
 badd +37 ../driver/src/f3d_delay.c
-badd +0 ../driver/inc/f3d_delay.h
-badd +0 ../driver/src/f3d_timer2.c
-badd +0 ../driver/inc/f3d_timer2.h
+badd +1 ../driver/inc/f3d_delay.h
+badd +42 ../driver/src/f3d_timer2.c
+badd +38 ../driver/inc/f3d_timer2.h
+badd +60 ~/classes/C335-Fall2017/lab5/main.c
 args main.c
 edit main.c
 set splitbelow splitright
@@ -35,13 +36,13 @@ setlocal fml=1
 setlocal fdn=20
 setlocal fen
 silent! normal! zE
-let s:l = 14 - ((13 * winheight(0) + 27) / 55)
+let s:l = 55 - ((38 * winheight(0) + 27) / 55)
 if s:l < 1 | let s:l = 1 | endif
 exe s:l
 normal! zt
-14
-normal! 023|
-tabedit Makefile
+55
+normal! 059|
+tabedit ../driver/inc/f3d_lcd_sd.h
 set splitbelow splitright
 set nosplitbelow
 set nosplitright
@@ -57,57 +58,35 @@ setlocal fml=1
 setlocal fdn=20
 setlocal fen
 silent! normal! zE
-let s:l = 10 - ((9 * winheight(0) + 27) / 55)
+let s:l = 97 - ((38 * winheight(0) + 27) / 55)
 if s:l < 1 | let s:l = 1 | endif
 exe s:l
 normal! zt
-10
-normal! 0218|
-tabedit ../driver/src/f3d_timer2.c
-set splitbelow splitright
-set nosplitbelow
-set nosplitright
-wincmd t
-set winheight=1 winwidth=1
-argglobal
-setlocal fdm=manual
-setlocal fde=0
-setlocal fmr={{{,}}}
-setlocal fdi=#
-setlocal fdl=0
-setlocal fml=1
-setlocal fdn=20
-setlocal fen
-silent! normal! zE
-let s:l = 42 - ((28 * winheight(0) + 27) / 55)
-if s:l < 1 | let s:l = 1 | endif
-exe s:l
-normal! zt
-42
-normal! 04|
-tabedit ../driver/inc/f3d_timer2.h
-set splitbelow splitright
-set nosplitbelow
-set nosplitright
-wincmd t
-set winheight=1 winwidth=1
-argglobal
-setlocal fdm=manual
-setlocal fde=0
-setlocal fmr={{{,}}}
-setlocal fdi=#
-setlocal fdl=0
-setlocal fml=1
-setlocal fdn=20
-setlocal fen
-silent! normal! zE
-let s:l = 38 - ((37 * winheight(0) + 27) / 55)
-if s:l < 1 | let s:l = 1 | endif
-exe s:l
-normal! zt
-38
+97
 normal! 0
-tabnext 2
+tabedit ../driver/src/f3d_lcd_sd.c
+set splitbelow splitright
+set nosplitbelow
+set nosplitright
+wincmd t
+set winheight=1 winwidth=1
+argglobal
+setlocal fdm=manual
+setlocal fde=0
+setlocal fmr={{{,}}}
+setlocal fdi=#
+setlocal fdl=0
+setlocal fml=1
+setlocal fdn=20
+setlocal fen
+silent! normal! zE
+let s:l = 2 - ((1 * winheight(0) + 27) / 55)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+2
+normal! 0
+tabnext 1
 if exists('s:wipebuf')
   silent exe 'bwipe ' . s:wipebuf
 endif
--- lab6/main.c
+++ lab6/main.c
@@ -14,33 +14,83 @@
 #include <f3d_lcd_sd.h>
 #include <stdio.h>
 
-#define TIMER 20000
+#define CHARSTEP 6 //the amount of space to put between characters
+#define BARWIDTH 12 //the width of the bar in the bar graph
+
+void drawString(int startx, int y, const char *str, uint16_t charColor, uint16_t charBGColor){
+    int i = 0;
+    while(str[i] != '\0'){
+        f3d_lcd_drawChar(startx + (i * CHARSTEP), y, str[i], charColor, charBGColor);
+        i++;
+    }
+}
+
+void bannerString(int speed, int y, const char *str, uint16_t strColor, uint16_t strBGColor){ //mess with this later
+    f3d_lcd_fillScreen2(strBGColor);
+    int i = 0;
+    int drawx = 0;
+    while(1){
+        if (drawx > ST7735_width -1) //for wrapping
+            drawx = 0;
+        //f3d_lcd_fillScreen2(strBGColor);
+        drawString(drawx , y, str, strColor, strBGColor);
+        i++;
+        drawx += speed;
+    }
+}
+
+
+
+void drawMiddleLine(uint16_t color){
+    int height = ST7735_height;
+    int width = ST7735_width;
+    int i = 0;
+    printf("%d\n", width / 2);
+    for(i; i < width; i++)
+        f3d_lcd_drawPixel(i, height / 2, color);
+
+}
+
+//bg color needed to 'erase' the part of the bar that shouldn't be showing
+void drawPositiveBar(float amount, uint16_t color, uint16_t bgColor){
+    
+}
+
 
 int main(void) {
-    // If you have your inits set up, this should turn your LCD screen red
+    //inits
+    f3d_uart_init();
+    f3d_gyro_init();
+    f3d_led_init();
+    f3d_user_btn_init();
     f3d_lcd_init();
+
+    f3d_lcd_fillScreen2(BLACK);
+    drawString(10, 10, "Hasan loves Katie!", RED, BLACK);
+
+    drawMiddleLine(RED);
+
     while(1){
 
 
-        f3d_lcd_drawChar(10, 10, 'H', RED, BLACK);
         /*
-        int TIME = 2000;
-        f3d_lcd_fillScreen2(RED);
-        delay(TIME);
-        f3d_lcd_fillScreen2(BLACK);
-        delay(TIME);
-        f3d_lcd_fillScreen2(BLUE);
-        delay(TIME);
-        f3d_lcd_fillScreen2(CYAN);
-        delay(TIME);
-        f3d_lcd_fillScreen2(GREEN);
-        delay(TIME);
-        f3d_lcd_fillScreen2(MAGENTA);
-        delay(TIME);
-        f3d_lcd_fillScreen2(YELLOW);
-        delay(TIME);
-        f3d_lcd_fillScreen2(WHITE);
-        */
+           int TIME = 2000;
+           f3d_lcd_fillScreen2(RED);
+           delay(TIME);
+           f3d_lcd_fillScreen2(BLACK);
+           delay(TIME);
+           f3d_lcd_fillScreen2(BLUE);
+           delay(TIME);
+           f3d_lcd_fillScreen2(CYAN);
+           delay(TIME);
+           f3d_lcd_fillScreen2(GREEN);
+           delay(TIME);
+           f3d_lcd_fillScreen2(MAGENTA);
+           delay(TIME);
+           f3d_lcd_fillScreen2(YELLOW);
+           delay(TIME);
+           f3d_lcd_fillScreen2(WHITE);
+           */
     }
 }
 

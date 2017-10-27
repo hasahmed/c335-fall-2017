fdeab3d3f6d4e1fbab4c1b03ee4a749818715b21
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-10-27 11:40:58 -0400

    did most of homework 3

---
+++ HW3/test.asm
@@ -0,0 +1,17 @@
+.text
+.syntax unified
+.thumb
+    ands r0, r1
+    eors r0, r2
+    lsls r0, r3
+    lsrs r0, r4
+    adds r0, r1, r2
+    eors r0, r3
+    lsrs r0, r2
+    asrs r0, r5
+    lsrs r0, r7
+    cmp r5, 100
+    cmp r0, 235
+    adds r1, 8
+    subs r1, 6
+    movs r7, 200
